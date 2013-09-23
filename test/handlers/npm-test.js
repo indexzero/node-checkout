dc781f5891bd4aa76c2051b84ddfabe814380ae4
indexzero <charlie.robbins@gmail.com>
2013-09-23 17:53:23 -0400

    [api test] Added `npm` handler to `node-checkout` with associated test.

---
+++ test/handlers/npm-test.js
@@ -0,0 +1,43 @@
+/*
+ * npm-test.js: Tests for npm checkout adapter.
+ *
+ * (C) 2012 Bradley Meck.
+ * MIT LICENSE
+ *
+ */
+
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    vows = require('vows'),
+    tmp = require('tmp'),
+    checkout = require('../..');
+
+vows.describe('npm').addBatch({
+  'checkout with a "type" of "npm"': {
+    topic: function () {
+      var callback = this.callback,
+          self     = this;
+
+      tmp.dir(function (err, dir) {
+        if (err) {
+          return callback(err);
+        }
+      
+        //
+        // Extra folder to check creation
+        //
+        self.dir = path.join(dir, 'npm-handler');
+        checkout({
+          type: 'npm',
+          package: 'checkout',
+          version: '0.0.0',
+          destination: self.dir
+        }, callback);
+      });
+    },
+    'should create new directories properly': function () {
+      assert(fs.statSync(path.join(this.dir, 'package.json')));
+    }
+  }
+}).export(module);
